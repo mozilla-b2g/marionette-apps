843c54ae98b0aff2f117b2e46fb230f19586f4f8
gaye <gaye@mozilla.com>
2013-07-24 21:11:43 -0700

    Small cleanup

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-// private api and interface
 var Apps = require('./lib/apps');
 
-/*
- * map of public modules to expose to the main interface.
- * key is module, value is function on the module
- */
-var PUBLIC = {
-  close: 'close',
-  launch: 'launch',
-  list: 'list',
-  switchtoapp: 'switchToApp'
-};
 
+/**
+ * @constructor
+ * @param {Apps} apps private marionette apps api.
+ */
 function MarionetteApps(apps) {
-  for (var key in PUBLIC) {
-    var methodName = PUBLIC[key];
+  for (var key in MarionetteApps.ModuleToFunction) {
+    var methodName = MarionetteApps.ModuleToFunction[key];
     var module = require('./lib/' + key);
     // bind the public interface to the object
     this[methodName] = module[methodName].bind(null, apps);
   }
 }
 
+/**
+ * Map of public modules to expose to the main interface.
+ * Key is module, value is function on the module.
+ */
+MarionetteApps.ModuleToFunction = {
+  close: 'close',
+  launch: 'launch',
+  list: 'list',
+  switchtoapp: 'switchToApp'
+};
+
+
+/**
+ * Create a new marionette apps plugin.
+ * @param {Marionette.Client} client marionette client to use.
+ * @param {Objects} options optional configuration.
+ * @return {MarionetteApps} new marionette apps.
+ */
 function setup(client, options) {
   var apps = Apps.setup(client, options);
-
-  // return public interface
   return new MarionetteApps(apps);
 }
 
